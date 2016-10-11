14e4c5c78b4a733e49a97d3ac0396f25ca770862
Sean Starnes <seanstar12@gmail.com>
2016-10-11 01:03:35 -0500

    updated model to use es-date transform

--- ember/app/models/post.js
+++ ember/app/models/post.js
@@ -4,7 +4,8 @@ export default DS.Model.extend({
   body: DS.attr(),
   title: DS.attr(),
   teaser: DS.attr(),
-  date: DS.attr('date'),
+  date: DS.attr('es-date'),
   categories: DS.attr(),
+  postId: DS.attr('number'),
 
 });
