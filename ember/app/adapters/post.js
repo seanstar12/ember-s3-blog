1cec743fcd9c904d71edefcdd31174570fce529e
Sean Starnes <seanstar12@gmail.com>
2016-10-12 01:21:51 -0500

    removed comments

--- ember/app/adapters/post.js
+++ ember/app/adapters/post.js
@@ -1,8 +1,5 @@
 import ES from 'ember-es-adapter/adapters/adapter';
 import config from 'ember-get-config';
-//import Ember from "ember";
-//import EsQuery from 'ember-es-adapter/utils/es-query-builder';
-//import fetch from "ember-network/fetch";
 
 export default ES.extend({
   host: config.EsAdapter.host,
