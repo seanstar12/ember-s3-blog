bdc7d1856bd21e923e4a23e85455549b9e9c8931
Sean Starnes <seanstar12@gmail.com>
2016-10-28 00:22:03 -0500

    added better DSL for elasticsearch

--- ember/app/routes/posts/index.js
+++ ember/app/routes/posts/index.js
@@ -4,6 +4,7 @@ import {QueryDSL} from 'ember-es-adapter/utils/es-tools';
 export default Ember.Route.extend({
   model(params) {
     params = params ? params : {};
+
     if (false) {
       params['esParams'] = {sort: 'date', sortType: 'desc'};
       params['backgroundReload'] = true;
@@ -12,23 +13,48 @@ export default Ember.Route.extend({
     }
     else {
       //simulated params
-      //params['size'] = 2;
-      //params['query'] = 'swagblazeit';
+      params['size'] = 10;
+      params['page'] = 0;
 
       //adding es params into the default params, and initiating new instance
 
-      let esParams = {'sort': 'date', 'sortType': 'desc'};
-      let dsl = new QueryDSL(esParams);
+      //let esParams = {'size': 15, 'sort': {'date':'desc'}};
+      //let dsl = new QueryDSL(esParams);
 
-      dsl.query()
-        .bool('must')
-        .match({'title': 'second'});
-        //.match_phrase({'title': 'second'});
+      if (false){
+        dsl.query()
+          .bool('must')
+          .match({'title': 'jerry'})
+          .term({'user': 'steve'});
 
-      console.log(dsl.getThis());
-      params['esQuery'] = {query:{match_all:{}},size: 20, sort: [{date:'desc'}]};
+        dsl.size(10).from(0);
+
+        dsl.filter()
+          .bool('must')
+          .match({'title': 'tim'});
+
+        dsl.highlight({fields:{content:{}}});
+
+        //dsl.highlight()
+        //  .fields({"content" : {}});
+
+        //dsl.query()
+        //  .match({"title": 'lenny'});
+
+        //dsl.query({match_all:{}})
+
+        //dsl.query({match_all:{}})
+        //  .sort(['date', {'title' : 'asc'}]);
+      }
+
+      let dsl = new QueryDSL(params);
+      dsl.query({match_all:{}})
+        .sort({'date': 'desc'});
+
+      params['esQuery'] = dsl.getQuery();
       return this.store.query('post', params);
     }
+      //params['esQuery'] = {query:{match_all:{}},size: 20, sort: [{date:'desc'}]};
     //return this.store.findAll('post', {page:0, size:10, backgroundReload: false});
 
   },
@@ -41,7 +67,7 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    error(error, transition) {
+    error(error) {
       if (error) {
         console.log(error);
         //return this.transitionTo('posts.error')
